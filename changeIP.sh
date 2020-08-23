069ad255689e70e97b80b60b0f658171c8e975b0
dolone <duxlong@me.com>
2020-08-23 13:07:52 +0800

    Update changeIP.sh

--- changeIP.sh
+++ changeIP.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-# changeIP.sh 放在 NAS 中 ;
-# 与 ip.txt 和 config.json 处于同一目录 ;
-# 用 ln 硬链接创建 config.json
+# 在NAS中运行changeIP.sh
 
-# 先进入操作目录
-cd /volume1/docker/better-cf-ip
+# 修改为自己的文件位置
+DIR_IP='/volume1/docker/better-cf-ip/ip.txt'
+DIR_CONFIG='/volume1/docker/v2fly/config.json'
 
 # 启动 better-cf-ip ; 得到结果后自动关闭
 docker start better-cf-ip
 
-# 等待5分钟，让 best-cf-ip 运行结束，得到 ip.txt
-sleep 5m
+# 等待3分钟，让 best-cf-ip 运行结束，得到 ip.txt
+sleep 3m
 
-# 5分钟后，如果还没运行完，说明网络状态都很差，就关闭 docker，使用原来的 ip
+# 3分钟后，如果还没运行完，说明网络状态都很差，就关闭 docker，使用原来的 ip
 docker stop better-cf-ip
 
 # 读取 ip.txt 并赋值给变量 ip
-ip=$(cat ./ip.txt)
+ip=$(cat $DIR_IP)
 
 # 修改v2ray config cf ip
 # sed 使用双引号才能识别变量 $ip ; 双引号内部的双引号需要转义
-sed -i "s/\(\"address\":\"\)\(.*\)\(\",\)/\1$ip\3/g" ./config.json
+sed -i "s/\(\"address\":\"\)\(.*\)\(\",\)/\1$ip\3/g" $DIR_CONFIG
 
 # 重启 v2ray 使配置生效
 docker restart v2fly
